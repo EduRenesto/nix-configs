ddeae2a83ed53b9a2578ec7e51f55a9626871a50
Edu Renesto <edure95@gmail.com>
2022-06-05 17:52:51 -0300

    nixos/alderaan: add pipewire

--- nixos/alderaan/configuration.nix
+++ nixos/alderaan/configuration.nix
@@ -87,7 +87,18 @@
         workstation = true;
       };
     };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
   };
+
+  security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
 
   system.stateVersion = "22.11";
 }
